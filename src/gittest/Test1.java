0756ac0d92280e8c1508badb3f71bd390ac37c72
15682349391 <382720942@qq.com>
2020-06-16 22:46:20 +0800

    再次修改

--- src/gittest/Test1.java
+++ src/gittest/Test1.java
@@ -2,12 +2,8 @@ package gittest;
 
 public class Test1 {
 	public static void main(String args[]) {
-<<<<<<< HEAD
-		System.out.println("hellogit");
 		System.out.println("test1的进一步修改");
-=======
 		System.out.println("hellogit2");
->>>>>>> branch 'master' of https://github.com/15682349391/Library.git
 	}
 
 }
